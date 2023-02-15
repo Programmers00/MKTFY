c39093f2bbd65f282ca7e2e06b578421a81c4309
eunshinNoh <nes0410@gmail.com>
2023-02-15 15:58:34 -0700

    refs #25  make trigger to close dropdown

--- src/components/dropdown/index.js
+++ src/components/dropdown/index.js
@@ -2,25 +2,24 @@ import { useState, useEffect, useRef } from "react";
 // scss
 import styles from "./index.module.scss";
 
-/** Dropdown: must get 2 children from parent, and speach buble tail */
+/** Dropdown: must get 2 children from parent, and isLeftTail, isRightTail, closeTrigger */
 /** example)
  * speachBubbleTail => "left" or "right",
  *  <div>children1</div>
  *  <div>children2</div>
  */
-const Dropdown = ({ children, isLeftTail, isRightTail }) => {
+const Dropdown = ({ children, isLeftTail, isRightTail, closeTrigger }) => {
   const [isShowMenu, setIsShowMenu] = useState(false);
-  const buttonRef = useRef();
+  const contentRef = useRef();
 
   /** click select box: when click the select box, open or close the menu */
   const handleInputClick = (e) => {
-    console.log("###input");
     setIsShowMenu(!isShowMenu);
   };
   // select label in menu: when click the item in menu, close the menu
   useEffect(() => {
     const handler = (event) => {
-      if (buttonRef.current && !buttonRef.current.contains(event.target)) {
+      if (contentRef.current && !contentRef.current.contains(event.target)) {
         setIsShowMenu(false);
       }
     };
@@ -31,33 +30,41 @@ const Dropdown = ({ children, isLeftTail, isRightTail }) => {
       window.removeEventListener("click", handler);
     };
   }, []);
+  // trigger to close the dropdown
+  useEffect(() => {
+    setIsShowMenu(false);
+  }, [closeTrigger]);
 
   return (
     <div className={styles.dropdownBox}>
-      <div ref={buttonRef} className={styles.buttonBox}>
+      <div
+        ref={contentRef}
+        className={styles.contentBox}
+        onClick={(event) => event.stopPropagation()}
+      >
         <button onClick={handleInputClick} className={styles.button}>
           {children[0]}
         </button>
+        {/* first children is for button */}
+        {/* tail left */}
+        {isShowMenu && isLeftTail && (
+          <div className={styles.menu}>
+            <div className={styles.menuContentTailLeft}>
+              {/* second children is for content */}
+              {children[1]}
+            </div>
+          </div>
+        )}
+        {/* tail right */}
+        {isShowMenu && isRightTail && (
+          <div className={styles.menu}>
+            <div className={styles.menuContentTailRight}>
+              {/* second children is for content */}
+              {children[1]}
+            </div>
+          </div>
+        )}
       </div>
-      {/* first children is for button */}
-      {/* tail left */}
-      {isShowMenu && isLeftTail && (
-        <div className={styles.menu}>
-          <div className={styles.menuContentTailLeft}>
-            {/* second children is for content */}
-            {children[1]}
-          </div>
-        </div>
-      )}
-      {/* tail right */}
-      {isShowMenu && isRightTail && (
-        <div className={styles.menu}>
-          <div className={styles.menuContentTailRight}>
-            {/* second children is for content */}
-            {children[1]}
-          </div>
-        </div>
-      )}
     </div>
   );
 };
--- src/layouts/header/welcomeDropdown/index.js
+++ src/layouts/header/welcomeDropdown/index.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 // scss
 import styles from "./index.module.scss";
@@ -6,9 +7,13 @@ import Dropdown from "../../../components/dropdown";
 // svg icon
 import { SingOutIcon } from "../../../assets/svgIcons";
 
-const welcomeDropdown = ({ userName }) => {
-  console.log("##userName", userName);
-  const myListingNotificationCount = 3;
+const WelcomeDropdown = ({ userName }) => {
+  /** data */
+  // trigger to close dropdown
+  const [closeTrigger, setCloseTrigger] = useState(0);
+  // notification
+  const myListingCount = 3;
+  // settings
   const settings = [
     { title: "Account Information", url: "/accountInformation" },
     { title: "Change password", url: "/changePassword" },
@@ -16,68 +21,74 @@ const welcomeDropdown = ({ userName }) => {
     {
       title: "My listings",
       url: "/myListings",
-      notification: myListingNotificationCount,
+      notification: myListingCount,
     },
   ];
+  // helps
   const help = [
     { title: "FAQ", url: "/faq" },
     { title: "Contact us", url: "/contactUs" },
   ];
-
+  /** sign out: signout and close trigger */
   const onSignOut = () => {
     console.log("##sign Out");
+    onCloseTrigger();
+  };
+
+  /** triger for close dropdown */
+  const onCloseTrigger = () => {
+    // always make new value=> trigger
+    setCloseTrigger(new Date());
   };
 
   return (
-    <>
-      <Dropdown isLeftTail>
-        {/* button */}
-        <div className={styles.userTitle}>
-          <span className={styles.welcome}>Welcome back</span>
+    <Dropdown isLeftTail closeTrigger={closeTrigger}>
+      {/* button */}
+      <div className={styles.userTitle}>
+        <span className={styles.welcome}>Welcome back</span>
+        <span className={styles.userName}>{userName}</span>
+      </div>
+      {/* content */}
+      <div className={styles.userContent}>
+        <div className={styles.infoBox}>
+          <div className={styles.circle}>{userName?.charAt(0)}</div>
           <span className={styles.userName}>{userName}</span>
         </div>
-        {/* content */}
-        <div className={styles.userContent}>
-          <div className={styles.infoBox}>
-            <div className={styles.circle}>{userName?.charAt(0)}</div>
-            <span className={styles.userName}>{userName}</span>
-          </div>
-          <div className={styles.contentBox}>
-            <span>Settings</span>
-            {settings.map((element) => (
-              <Link key={element.title} to={element.url}>
-                <p>{element.title}</p>
-                {element.notification && (
-                  <div className={styles.notification}>
-                    {element.notification}
-                  </div>
-                )}
-              </Link>
-            ))}
-          </div>
-          <div className={styles.contentBox}>
-            <span>Help</span>
-            {help.map((element) => (
-              <Link key={element.title} to={element.url}>
-                <p>{element.title}</p>
-                {element.notification && (
-                  <div className={styles.notification}>
-                    {element.notification}
-                  </div>
-                )}
-              </Link>
-            ))}
-          </div>
-          <div className={styles.signOutBox} onClick={onSignOut}>
-            <a>
-              <p>Sign Out</p>
-              <SingOutIcon />
-            </a>
-          </div>
+        <div className={styles.contentBox}>
+          <span>Settings</span>
+          {settings.map((element) => (
+            <Link onClick={onCloseTrigger} key={element.title} to={element.url}>
+              <p>{element.title}</p>
+              {element.notification && (
+                <div className={styles.notification}>
+                  {element.notification}
+                </div>
+              )}
+            </Link>
+          ))}
         </div>
-      </Dropdown>
-    </>
+        <div className={styles.contentBox}>
+          <span>Help</span>
+          {help.map((element) => (
+            <Link onClick={onCloseTrigger} key={element.title} to={element.url}>
+              <p>{element.title}</p>
+              {element.notification && (
+                <div className={styles.notification}>
+                  {element.notification}
+                </div>
+              )}
+            </Link>
+          ))}
+        </div>
+        <div className={styles.signOutBox} onClick={onSignOut}>
+          <a>
+            <p>Sign Out</p>
+            <SingOutIcon />
+          </a>
+        </div>
+      </div>
+    </Dropdown>
   );
 };
 
-export default welcomeDropdown;
+export default WelcomeDropdown;
